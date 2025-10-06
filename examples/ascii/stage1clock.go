b3cca229fe094029f2dc211c7f8053be3fdb1755
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-10-06 21:17:48 +0200

    ascii 4 - clock hand adjust

--- examples/ascii/stage1clock.go
+++ examples/ascii/stage1clock.go
@@ -3,6 +3,7 @@ package main
 import (
     "log"
 	"time"
+	"math"
     "github.com/hajimehoshi/ebiten/v2/ebitenutil"
     "image/color"
 	"github.com/hajimehoshi/ebiten/v2"
@@ -11,18 +12,25 @@ import (
 func stage1clock(screen *ebiten.Image){
     duration := time.Since(startTime)
     milliseconds := duration.Milliseconds()
-    y := 1000 - milliseconds/10
+    y := float64(800 - milliseconds/20)
     //log.Printf("Outro will be displayed at y=%f", y)
     animateText(screen, clock, 20, 10, float64(y))
 
-    x2 := 850.5
+    x2 := 820.1
     y2 := float64(y + 250)
-    x1 := float64(400)
+    x1 := float64(690)
     thickness := 4
     col := color.RGBA{200, 20, 20, 255}
 
+    r := 211.63/2
+
+    alpha := (float64(y) * 6.28) / 800
+
+    xCir := r*math.Sin(alpha)
+    yCir := r*math.Cos(alpha)
+
     for i := -int(thickness / 2); i <= int(thickness/2); i++ {
-        ebitenutil.DrawLine(screen, x1+float64(i), float64(y+83)+float64(i), x2+float64(i), y2+float64(i), col)
+        ebitenutil.DrawLine(screen, x1+xCir+float64(i), float64(y+83)+float64(i)+yCir, x2+float64(i), y2+float64(i), col)
     }
 
     log.Printf("", x1, y, x2, y2)
