d99d7ed22aa36aacaf961d4d4bf293a8c33f7565
kdrzazga <krzysztof.drzazga@tutanota.com>
2025-10-06 18:51:29 +0200

    ascii 3 - clock hand attempt

--- examples/ascii/stage1clock.go
+++ examples/ascii/stage1clock.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+    "log"
 	"time"
+    "github.com/hajimehoshi/ebiten/v2/ebitenutil"
+    "image/color"
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
@@ -11,4 +14,16 @@ func stage1clock(screen *ebiten.Image){
     y := 1000 - milliseconds/10
     //log.Printf("Outro will be displayed at y=%f", y)
     animateText(screen, clock, 20, 10, float64(y))
+
+    x2 := 500.5
+    y2 := float64(y) + float64(50)
+    x1 := float64(160)
+    thickness := 4
+    col := color.RGBA{200, 20, 20, 255}
+
+    for i := -int(thickness / 2); i <= int(thickness/2); i++ {
+        ebitenutil.DrawLine(screen, x1+float64(i), float64(y)+float64(i), x2+float64(i), y2+float64(i), col)
+    }
+
+    log.Printf("", x1, y, x2, y2)
 }
